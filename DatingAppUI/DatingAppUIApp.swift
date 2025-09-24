3e6cbe3de2e065fd599d7dddb39aeb2a796d6a50
arwysyah <arwysyahputrasiregar@gmail.com>
2025-09-24 23:38:16 +0700

    Initial Commit

---
+++ DatingAppUI/DatingAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatingAppUIApp.swift
+//  DatingAppUI
+//
+//  Created by Arwy Syahputra Siregar on 24/09/25.
+//
+
+import SwiftUI
+
+@main
+struct DatingAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
